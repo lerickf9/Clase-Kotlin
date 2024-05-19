f13ad53ab96f542278367d81917dd495e09dca7a
Erick <efuentes.edf9@gmail.com>
2024-05-19 02:08:04 -0400

    Subiendo primeros archivos

---
+++ app/src/main/java/com/erickcode/androidfirst/Variables.kt
@@ -0,0 +1,24 @@
+package com.erickcode.androidfirst
+
+
+fun main(){
+    /*
+    Variables Numericas
+     */
+
+    //Int -2.147.483.647 a 2.147.483.647
+    val age:Int= 30
+
+    //Long > Int  -9.223.036.854.775.807 a 9.223.036.854.775.807
+    val example:Long = 30000;
+
+    //Float
+    val floatExample:Float = 30.5f
+
+    //Double
+    val doubleExample:Double = 30.5565545
+
+
+
+}
+
