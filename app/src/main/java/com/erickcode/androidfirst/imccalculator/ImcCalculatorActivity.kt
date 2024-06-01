e5c723b4fe8ef91d1cb302a081fad47703a7bbac
Erick <efuentes.edf9@gmail.com>
2024-06-01 00:22:47 -0400

    Add card weigth

--- app/src/main/java/com/erickcode/androidfirst/imccalculator/ImcCalculatorActivity.kt
+++ app/src/main/java/com/erickcode/androidfirst/imccalculator/ImcCalculatorActivity.kt
@@ -5,6 +5,7 @@ import android.os.Build
 import android.os.Bundle
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.FloatRange
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
@@ -12,18 +13,24 @@ import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.erickcode.androidfirst.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.slider.RangeSlider
 
 class ImcCalculatorActivity : AppCompatActivity() {
 
-    private var isMaleSeleted:Boolean= true
-    private var isFemaleSeleted:Boolean= false
+    private var isMaleSeleted: Boolean = true
+    private var isFemaleSeleted: Boolean = false
+    private var currentWeigth: Int = 70
 
 
-    private lateinit var viewMale:CardView
-    private lateinit var viewFemale:CardView
-    private lateinit var tvHeight:TextView
+    private lateinit var viewMale: CardView
+    private lateinit var viewFemale: CardView
+    private lateinit var tvHeight: TextView
     private lateinit var rsHeight: RangeSlider
+    private lateinit var btnbtnSubstractWeight: FloatingActionButton
+    private lateinit var btnPlusWeight: FloatingActionButton
+    private lateinit var tvWeiht: TextView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,52 +47,70 @@ class ImcCalculatorActivity : AppCompatActivity() {
     }
 
 
-
-    private fun initComponents(){
+    private fun initComponents() {
         viewMale = findViewById(R.id.viewMale)
         viewFemale = findViewById(R.id.viewFemale)
         tvHeight = findViewById(R.id.tvHeight)
         rsHeight = findViewById(R.id.rsHeight)
+        btnbtnSubstractWeight = findViewById(R.id.btnSubstractWeight)
+        btnPlusWeight = findViewById(R.id.btnPlusWeight)
+        tvWeiht = findViewById(R.id.tvWeight)
 
     }
+
     @RequiresApi(Build.VERSION_CODES.N)
     private fun initListeners() {
-        viewMale.setOnClickListener{
+        viewMale.setOnClickListener {
             changeGender()
             setGenderColor()
         }
-        viewFemale.setOnClickListener{
+        viewFemale.setOnClickListener {
             changeGender()
             setGenderColor()
         }
-        rsHeight.addOnChangeListener{ _, value, _ ->
+        rsHeight.addOnChangeListener { _, value, _ ->
             val df = DecimalFormat("#.##")
             val result = df.format(value)
-            tvHeight.text = "$result cm"
+            tvHeight.text = result + " cm"
         }
+        btnPlusWeight.setOnClickListener {
+            currentWeigth += 1
+            setWeigth()
+        }
+        btnbtnSubstractWeight.setOnClickListener {
+            currentWeigth -= 1
+            setWeigth()
+        }
+
     }
-    private fun changeGender(){
+
+    private fun setWeigth() {
+        tvWeiht.text = currentWeigth.toString()
+    }
+
+    private fun changeGender() {
         isMaleSeleted = !isMaleSeleted
         isFemaleSeleted = !isFemaleSeleted
     }
 
-    private fun setGenderColor(){
+    private fun setGenderColor() {
 
         viewMale.setCardBackgroundColor(getBackgroundColor(isMaleSeleted))
         viewFemale.setCardBackgroundColor(getBackgroundColor(isFemaleSeleted))
     }
 
-    private fun getBackgroundColor(isSelectedComponent: Boolean): Int{
+    private fun getBackgroundColor(isSelectedComponent: Boolean): Int {
 
-        val colorReference = if(isSelectedComponent){
+        val colorReference = if (isSelectedComponent) {
             R.color.background_component_selected
-        }else{
+        } else {
             R.color.background_component
         }
         return ContextCompat.getColor(this, colorReference)
     }
+
     private fun initUI() {
         setGenderColor()
-
+        setWeigth()
     }
 }
