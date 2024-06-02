721f6275a7fc1a3b39afc881e5a5a81d99109a9b
Erick <efuentes.edf9@gmail.com>
2024-06-02 00:15:30 -0400

    Proyecto finished

--- app/src/main/java/com/erickcode/androidfirst/imccalculator/ResultIMCActivity.kt
+++ app/src/main/java/com/erickcode/androidfirst/imccalculator/ResultIMCActivity.kt
@@ -1,9 +1,11 @@
 package com.erickcode.androidfirst.imccalculator
 
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.erickcode.androidfirst.R
@@ -14,6 +16,8 @@ class ResultIMCActivity : AppCompatActivity() {
     private lateinit var tvResult: TextView
     private lateinit var tvIMC: TextView
     private lateinit var tvDescripcion: TextView
+    private lateinit var btnRecalculate: Button
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,32 +31,41 @@ class ResultIMCActivity : AppCompatActivity() {
         val result = intent.extras?.getDouble(IMC_KEY) ?: -1.0
         initComponents()
         initUI(result)
+        initListener()
+    }
+
+    private fun initListener() {
+        btnRecalculate.setOnClickListener{ onBackPressedDispatcher }
     }
 
     private fun initUI(result:Double) { // Bajo peso
+        tvIMC.text = result.toString()
         when(result){
             in 0.00..18.50 ->{ // Bajo peso
-                tvIMC
-                tvResult
-                tvDescripcion
+                tvResult.text = getString(R.string.title_peso_bajo)
+                tvResult.setTextColor(ContextCompat.getColor(this, R.color.bajo_peso))
+                tvDescripcion.text = getString(R.string.descripcion_peso_bajo)
             }
             in 18.51..24.99 ->{ // Normal
-                tvIMC
-                tvResult
-                tvDescripcion
+                tvResult.text = getString(R.string.title_peso_normal)
+                tvResult.setTextColor(ContextCompat.getColor(this, R.color.peso_normal))
+                tvDescripcion.text = getString(R.string.descripcion_peso_normal)
             }
             in 25.00..29.99 ->{ // Sobrepeso
-                tvIMC
-                tvResult
-                tvDescripcion
+                tvResult.text = getString(R.string.title_sobrepeso)
+                tvResult.setTextColor(ContextCompat.getColor(this, R.color.sobre_peso))
+                tvDescripcion.text = getString(R.string.descripcion_sobrepeso)
             }
             in 30.00..99.00 ->{ // Obesidad
-
+                tvResult.text = getString(R.string.title_obesidad)
+                tvResult.setTextColor(ContextCompat.getColor(this, R.color.obesidad))
+                tvDescripcion.text = getString(R.string.descripcion_obesidad)
             }
             else -> {
-                tvIMC.text = ("Error")
-                tvResult.text = ("Error")
-                tvDescripcion.text = ("Error")
+                tvIMC.text = getString(R.string.error)
+                tvResult.text = getString(R.string.error)
+                tvResult.setTextColor(ContextCompat.getColor(this, R.color.obesidad))
+                tvDescripcion.text = getString(R.string.error)
             }
         }
     }
@@ -61,5 +74,6 @@ class ResultIMCActivity : AppCompatActivity() {
         tvIMC = findViewById(R.id.tvIMC)
         tvResult = findViewById(R.id.tvResult)
         tvDescripcion = findViewById(R.id.tvDescripcion)
+        btnRecalculate = findViewById(R.id.btnReCalculate)
     }
 }
