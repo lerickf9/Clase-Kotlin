fb5c95d52bb24da1ecaff48a978e98b9402642ba
Erick <efuentes.edf9@gmail.com>
2024-06-08 23:08:15 -0400

    feat: Startding retrofit in dependecies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,9 +44,18 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+
+
+    //retrofit
+
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+
 }
--- app/src/main/java/com/erickcode/androidfirst/superheroapp/SuperHeroListActivity.kt
+++ app/src/main/java/com/erickcode/androidfirst/superheroapp/SuperHeroListActivity.kt
@@ -8,20 +8,35 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.erickcode.androidfirst.R
 import com.erickcode.androidfirst.databinding.ActivitySuperHeroListBinding
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class SuperHeroListActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySuperHeroListBinding
+    private lateinit var retrofit: Retrofit
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySuperHeroListBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        retrofit = getRetrofit()
         initUI()
 
     }
 
+//    private fun getInfo(){
+    //    https://gateway.marvel.com/v1/public/characters?ts=1&apikey=ac2fde71ec4fa2f0dcd9245812e75366&hash=124d1ddc4155cc9ae5df35d093307474
+////        URL url=null
+////        private var = namePerson.getText().toString()
+//        var urlSearch = HOSTNAME + SERVICE + "ts=" + ts + "&apikey=" +
+//            PUBLIC_API_KEY + "&hash=" + HASH + "&name" + name
+//        urlSearch = urlSearch.replace("http:", "https:")
+//
+//    }
+
     private fun initUI() {
-        binding.searchView.setOnQueryTextListener(object:SearchView.OnQueryTextListener{
+        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String?): Boolean {
                 searchByName(query.orEmpty())
                 return false
@@ -31,10 +46,21 @@ class SuperHeroListActivity : AppCompatActivity() {
                 return false
             }
 
+
         })
     }
 
     private fun searchByName(query: String) {
 
     }
+
+
+    private fun getRetrofit(): Retrofit {
+        return Retrofit
+            .Builder()
+            .baseUrl("https://gateway.marvel.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
 }
